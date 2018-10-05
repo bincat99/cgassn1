05e3c5afcbfd1d1f1e1dc88ab9c8ee98d5dc17cd
Jang WonJong <wonjong@postech.ac.kr>
2018-10-05 17:49:21 +0900

    item animation updating...

--- cgassn1/item.cpp
+++ cgassn1/item.cpp
@@ -36,6 +36,20 @@ void Item::display()
     glEnd();
 }
 
+void Item::LoadTexture(unsigned int idx)
+{
+	BmpLoader * tmp = bl[idx];
+
+	glGenTextures(1, &textureID);
+	glBindTexture(GL_TEXTURE_2D, textureID);
+	glPixelStorei(GL_UNPACK_ALIGNMENT, 1);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+	gluBuild2DMipmaps(GL_TEXTURE_2D, GL_RGB, tmp->iWidth, tmp->iHeight, GL_RGB, GL_UNSIGNED_BYTE, tmp->textureData);
+}
+
 Item::~Item ()
 {
     //printf ("used!\n");
