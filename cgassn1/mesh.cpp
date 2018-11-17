b619e5760731a933696d633bdbf468bb2048298c
Jang WonJong <wonjong@postech.ac.kr>
2018-11-17 19:49:25 +0900

    animation added

--- cgassn1/mesh.cpp
+++ cgassn1/mesh.cpp
@@ -164,8 +164,9 @@ void Mesh::recursiveNodeProcess(aiNode * node)
 		}
 		else if (std::find(tokens.begin(), tokens.end(), "dummy_rpelvis") != tokens.end()) {
 			glm::mat4 temp = glm::mat4(1.0f);
+			memcpy(current_matrix, glm::value_ptr(temp), sizeof(current_matrix));
 			saveMatrix(0);
-			glm::mat4 temp = glm::translate(glm::mat4(1.0f), glm::vec3(-8, 90, 0))*glm::rotate(glm::mat4(1.0), -(3.14f / 4), glm::vec3(1, 0, 0))* glm::translate(glm::mat4(1.0f), glm::vec3(8, -90, 0));
+			temp = glm::translate(glm::mat4(1.0f), glm::vec3(-8, 90, 0))*glm::rotate(glm::mat4(1.0), -(3.14f / 4), glm::vec3(1, 0, 0))* glm::translate(glm::mat4(1.0f), glm::vec3(8, -90, 0));
 			memcpy(current_matrix, glm::value_ptr(temp), sizeof(current_matrix));
 			saveMatrix(1);
 			saveMatrix(2);
