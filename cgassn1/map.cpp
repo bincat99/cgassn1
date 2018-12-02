7ecec5699711ba9e5a92c5f91431e00c33fccb05
imnotkind <billy307@daum.net>
2018-12-02 10:30:49 +0900

    for saving

--- cgassn1/map.cpp
+++ cgassn1/map.cpp
@@ -437,7 +437,8 @@ Map::display(void)
 
 	//Wall
 	shaderWallUtil.Use();
-
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+	glEnable(GL_DEPTH_TEST);
 
 
 	if (!(player->status == KILLED || gameClear))
